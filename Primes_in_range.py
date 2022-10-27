3cc6205dc7beee1eeda19b51ce258a20e3c06361
GOKULKRISHNAUDATA <85216577+GOKULKRISHNAUDATA@users.noreply.github.com>
2022-10-27 22:07:26 +0530

    import math def isprime(n):     if n<=1:         return False     for i in range(2,int(math.sqrt(n))+1):         if n%i==0:             return False     return True a=int(input()) b=int(input()) cnt=0 for i in range(a,b+1):     if isprime(i):         cnt+=1 print(cnt)

---
+++ Primes_in_range.py
@@ -0,0 +1,15 @@
+import math 
+def isprime(n):
+    if n<=1:
+        return False
+    for i in range(2,int(math.sqrt(n))+1):
+        if n%i==0:
+            return False
+    return True
+a=int(input())
+b=int(input())
+cnt=0
+for i in range(a,b+1):
+    if isprime(i):
+        cnt+=1
+print(cnt)        
